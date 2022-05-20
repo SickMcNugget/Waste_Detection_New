9226477b90d9ab10aa543f744265f831a90c277f
Stab579 <jnregan00@hotmail.com>
2022-05-20 19:10:04 +0800

    Removed unneeded entries from evaluation script

--- F_RCNN/eval.py
+++ F_RCNN/eval.py
@@ -17,15 +17,10 @@ cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 cfg.DATASETS.TRAIN = ("trash_train",)
 cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
+cfg.DATALOADER.NUM_WORKERS = 8
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 16  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1
 
 predictor = DefaultPredictor(cfg)
 
