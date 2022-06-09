17ff4612a1d77745dada461de57b4da38bd37233
Stab579 <jnregan00@hotmail.com>
2022-06-09 18:18:21 +0800

    Modified to find my custom datasets

--- DETR/datasets/coco.py
+++ DETR/datasets/coco.py
@@ -149,8 +149,8 @@ def build(image_set, args):
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "train", root / "train" / '_annotations.coco.json'),
+        "val": (root / "test", root / "test" / '_annotations.coco.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
