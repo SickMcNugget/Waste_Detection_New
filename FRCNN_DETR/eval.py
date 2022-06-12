8978c76b65e30e935af85b05e96d212799c3c9e2
Stab579 <jnregan00@hotmail.com>
2022-06-12 19:45:17 +0800

    Updated to work with new setup objects

--- FRCNN_DETR/eval.py
+++ FRCNN_DETR/eval.py
@@ -6,21 +6,32 @@ from detectron2.engine import DefaultPredictor
 from detectron2 import model_zoo
 import os
 import argparse
-from waste_utils import get_cfg_defaults
+from waste_utils import DetrSetup, FasterRCNNSetup, register_waste_dataset
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluate a model from a given directory")
+    parser.add_argument('type', type=str, help='what type of model is it')
     parser.add_argument('path', type=str, help='The path to evaluate')
 
     #Get the path to the model's directory
     args = parser.parse_args()
 
-    #Make sure the data is visible
-    cfg = get_cfg_defaults()
+    trainer = None
+    if args.type == 'frcnn':
+        trainer = FasterRCNNSetup(args)
+    elif args.type == 'detr':
+        trainer = DetrSetup(args)
+    else:
+        return
+
+    register_waste_dataset()
+
+    trainer.get_cfg_defaults()
+    cfg = trainer.cfg
 
     # Ensure the correct datasets are tested
-    cfg.DATASETS.TRAIN = ("trash_train_9-COCO_raw",)
-    cfg.DATASETS.TEST = ("trash_test_9-COCO_raw",)
+    cfg.DATASETS.TRAIN = ("trash_train_10-COCO_raw",)
+    cfg.DATASETS.TEST = ("trash_test_10-COCO_raw",)
 
     predictor = DefaultPredictor(cfg)
     DetectionCheckpointer(predictor.model).load(args.path + "model_final.pth")
