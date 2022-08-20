37afbab64bfe8f7edc648acab279a3321c33fead
SickMcNugget <jnregan00@hotmail.com>
2022-08-21 01:03:47 +0800

    Added show functionality, added more args

--- YOLOV5/vis.py
+++ YOLOV5/vis.py
@@ -1,63 +1,66 @@
 import math
 from multiprocessing import Pool
 import os
-from turtle import color
-from unicodedata import category
+from pydoc import describe
 import numpy as np
-import matplotlib
-# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from PIL import Image
 from pycocotools.coco import COCO
 import random
-import sys
 import argparse
+import sys
 
 def main():
 
     # User input will determine the total number of images to save
     parser = argparse.ArgumentParser(description='Annotate and save a chosen number of images')
-    parser.add_argument('number', metavar='N', type=int,
+    parser.add_argument('--number', default=1, metavar='NUM', type=int,
                         help='The number of images to save')
-    parser.add_argument('--data-path', help="The directory containing COCO images")
-    parser.add_argument('--ann-path', default=args.data_path + "_annotations.coco.json",
+    parser.add_argument('--data-path', default=".", metavar='DATA', help="The directory containing COCO images")
+    parser.add_argument('--ann-path', metavar='ANN',
                         help='The annotation file for the data path')
+    parser.add_argument('--show', help="Shows the plot instead of exporting the image", action='store_true')
     args = parser.parse_args()
-
-    print(args.ann_path)
-
+    
     #The location of the images and annotations
-    data_path = "../datasets/TACO_TN_UAV_9-COCO_raw/train/"
-    coco_annotation_file_path = data_path + "_annotations.coco.json"
+    full_annotation_path = ""
+    if args.ann_path is not None:
+        full_annotation_path = args.ann_path
+    else:
+        full_annotation_path = os.path.join(args.data_path, "_annotations.coco.json")
 
-    coco_annotation = COCO(annotation_file=coco_annotation_file_path)
+    if os.path.exists(os.path.abspath(full_annotation_path)):
+        coco_annotation = COCO(annotation_file=full_annotation_path)
 
-    # Category IDs
-    cat_ids = coco_annotation.getCatIds()
+        # Category IDs
+        cat_ids = coco_annotation.getCatIds()
+        assert len(cat_ids) != 0
 
-    # All categories
-    cats = coco_annotation.loadCats(cat_ids)
-    cat_names = [cat["name"] for cat in cats]
-    img_ids = coco_annotation.getImgIds()
+        # All categories
+        cats = coco_annotation.loadCats(cat_ids)
+        cat_names = [cat["name"] for cat in cats]
+        img_ids = coco_annotation.getImgIds()
 
-    # Multiprocessing to save images faster
-    total_pools = os.cpu_count()
-    # Allows <total_pools> number of images to be saved simultaneously
-    total_loops = math.ceil(args.number / total_pools)
-    img_ids = random.sample(img_ids, args.number)
+        # Multiprocessing to save images faster
+        total_pools = os.cpu_count()
+        # Allows <total_pools> number of images to be saved simultaneously
+        total_loops = math.ceil(args.number / total_pools)
+        img_ids = random.sample(img_ids, args.number)
 
-    for i in range(total_loops):
-        pooldata = [
-            (img_ids[img_id], coco_annotation, data_path, cat_names) 
-                for img_id in range(i*total_pools, min(total_pools * (i+1), args.number))
-            ]
-        with Pool(processes=total_pools) as pool:
-            pool.starmap(save_random, pooldata)
+        # Processes and saves one image at a time
+        for i in range(total_loops):
+            pooldata = [
+                (img_ids[img_id], coco_annotation, args.data_path, cat_names, args) 
+                    for img_id in range(i*total_pools, min(total_pools * (i+1), args.number))
+                ]
+            with Pool(processes=total_pools) as pool:
+                pool.starmap(annotate_and_save, pooldata)
 
-def clamp(n, smallest, largest):
-    return max(smallest, min(n, largest))
-
-def save_random(img_id, coco_annotation, data_path, cat_names):
+def annotate_and_save(img_id, coco_annotation, data_path, cat_names, args):
+    '''Uses a COCO image ID and annotation file to load in an image
+    and then annotate the image with bounding boxes. The result is then
+    saved to disk
+    '''
     img_info = coco_annotation.loadImgs([img_id])[0]
     img_file_name = img_info["file_name"]
 
@@ -76,7 +79,10 @@ def save_random(img_id, coco_annotation, data_path, cat_names):
     # Plot segmentation and bounding box.
     coco_annotation.showAnns(anns, draw_bbox=True)
     put_text(cat_names, anns, np.asarray(im))
-    fig.savefig(f"imgs/{img_id}_annotated.png", dpi=600, transparent=True)
+    if args.show:
+        plt.show()
+    else:
+        fig.savefig(f"imgs/{img_id}_annotated.jpg", dpi=600, bbox_inches="tight", pad_inches=0)
 
 def put_text(cat_names, anns, im):
     font = {
